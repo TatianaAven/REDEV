253f6579bb9c58acdc444d316c82bdc9fde56b55
Tatiana A <litlegirl26@gmail.com>
2025-03-12 21:44:18 +0300

    второй чек

---
+++ задачи REDEV/1_check_list/1.2.js
@@ -0,0 +1,10 @@
+//1. Чему будет равно выражение `typeof -5;` ?
+console.log(typeof -5); //number
+//2. Чему будет равно выражение `typeof false;` ?
+console.log(typeof false); //boolean
+//3. Чему будет равно выражение `typeof "1";` ?
+console.log(typeof '1'); //string
+//4. Чему будет равно выражение `typeof null;` ?
+console.log(typeof null); //object косяк JS
+//5. Чему будет равно выражение `typeof typeof 5;` ? и почему?
+console.log(typeof typeof -5); //string
