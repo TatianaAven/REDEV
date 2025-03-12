0c20c28251b3a1fdab261ae1b703a584c47c43cc
Tatiana A <litlegirl26@gmail.com>
2025-03-12 21:40:22 +0300

    первый чек

---
+++ задачи REDEV/1_check_list/1.1.js
@@ -0,0 +1,30 @@
+//1. Создай две переменные с именами name и age и присвой им любые значения.
+let name = 'Tanya';
+let ageMy = 33;
+//2. Создай переменную nameAge и присвой ей значение, равное объединению(+) значений переменных name и age.
+let nameAge = name + ageMy;
+//3. Выведи переменную nameAge в консоль.
+console.log(nameAge);
+//3. Что выведет в консоль ?
+const studentName = "John";
+studentName = "Paul";
+console.log(studentName); //ошибка это const
+
+var age = 25;
+var age = 26;
+console.log(age);//26
+
+let age = 25;
+let age = 26;
+console.log(age);//ошибка переменная с тем же именем
+
+let age = 25;
+console.log(age);//25
+age = 30;
+console.log(age);//30
+
+console.log(age);//ошибка
+let age = 25;
+
+console.log(age);//underfined
+var age = 25;
